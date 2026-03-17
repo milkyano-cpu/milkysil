9d3a7e19bf64bd82701f0d065f68ec15a50df7fb
milkyano-cpu <dummyforfadedline@gmail.com>
2026-03-17 17:27:59 +0700

    Update : Route Api Category

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -2,6 +2,14 @@ import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export async function GET() {
-  const categories = await prisma.category.findMany()
+  const categories = await prisma.category.findMany({
+    include: {
+      products: true, 
+    },
+    orderBy: {
+      id: "asc",
+    },
+  })
+
   return NextResponse.json(categories)
 }
