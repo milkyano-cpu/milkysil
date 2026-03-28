ef627ca9e0d2f774c30dc8004c64031869b4795c
harebuss69 <devteam5@hare-business.com>
2026-03-29 00:33:09 +0700

    change pre existing specs

--- lib/constants/product-specifications.ts
+++ lib/constants/product-specifications.ts
@@ -1,33 +1,22 @@
 /**
- * Predefined specification keys for chemical products
+ * Predefined specification keys for products
  * Used in the specification editor to provide autocomplete suggestions
  */
 export const PREDEFINED_SPECIFICATION_KEYS = [
-  "Kemurnian",
-  "Konsentrasi",
-  "pH Level",
-  "Densitas",
-  "Viskositas",
-  "Berat Molekul",
-  "Titik Didih",
-  "Titik Leleh",
-  "Titik Beku",
-  "Kelarutan",
-  "Kemasan",
-  "Negara Asal",
-  "Warna",
-  "Bau",
-  "Bentuk",
+  "Type",
+  "Form",
+  "Function",
+  "Surface",
+  "Usage",
 ] as const
 
 /**
  * Common specification keys pre-populated when creating a new product
  */
 export const COMMON_SPECIFICATION_KEYS = [
-  "Kemurnian",
-  "Konsentrasi",
-  "pH Level",
-  "Kemasan",
-  "Negara Asal",
-  "Warna",
+  "Type",
+  "Form",
+  "Function",
+  "Surface",
+  "Usage",
 ] as const
